b3b1906d1ec4a9897bac75cf1d1a3046c26ab887
Salini <shalini021037@gmail.com>
2022-09-01 09:22:19 +0530

    file added

---
+++ Displaydata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseAssignmnets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Repos\PracticeProject\student.txt";
+            var lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+
+            Console.ReadLine();
+        }
+    }
+}
