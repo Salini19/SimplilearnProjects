e77dfec6f8229c150881cff9ed6b05aba6686276
Salini19 <111352466+Salini19@users.noreply.github.com>
2022-09-13 22:12:04 +0530

    Project3 added

---
+++ SortandSearch.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO; 
+using System.Threading.Tasks;
+
+namespace PractiseAssignmnets
+{
+    internal class RetriveData
+    {
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+            public string Grade { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+            string[] lines = File.ReadAllLines(@"E:\Repos\s.txt");
+          
+            List<Student> list = new List<Student>();
+            foreach (string student in lines)
+            {
+
+                Student stud = new Student();
+                string[] splitdata = student.Split(' ');
+                stud.StudentID = Convert.ToInt32(splitdata[0]);
+                stud.StudentName = splitdata[1];
+                stud.Age= Convert.ToInt32(splitdata[2]);
+                stud.Grade = splitdata[3];
+                list.Add(stud);
+
+
+            }
+            //Console.WriteLine("Student Details");
+            //Console.WriteLine();
+            //foreach (var item in list)
+            //{
+
+
+            //    Console.WriteLine("StudentID   : " + item.StudentID);
+            //    Console.WriteLine("StudnetName : " + item.StudentName);
+            //    Console.WriteLine("AGE         : " + item.Age);
+            //    Console.WriteLine("Grade       : " + item.Grade);
+            //    Console.WriteLine();
+
+            //}
+
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Sorted List");
+            Console.ForegroundColor = ConsoleColor.White;
+            var SortedList = list.OrderBy(s => s.StudentName);
+
+            Console.WriteLine();
+            foreach (var item in SortedList)
+            {
+
+
+                Console.WriteLine("StudentID   : " + item.StudentID);
+                Console.WriteLine("StudnetName : " + item.StudentName);
+                Console.WriteLine("Age         : " + item.Age);
+                Console.WriteLine("Grade       : " + item.Grade);
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("****************************");
+            Console.WriteLine("Enter student name u Need to search");
+            string name = Console.ReadLine();
+
+            var search = list.FindAll(f => f.StudentName == name);
+            foreach (var item in search)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("StudentID   : " + item.StudentID);
+                Console.WriteLine("StudnetName : " + item.StudentName);
+                Console.WriteLine("Age         : " + item.Age);
+                Console.WriteLine("Grade       : " + item.Grade);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
